f57febab94d6825ea2fe5afe4412ab205ed86bd6
cgusb <cgusbecker@gmail.com>
2022-05-12 23:28:27 -0400

    Add snail trails

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,87 @@
+let nPixelsWidth = 500
+let nPixelsHeight = nPixelsWidth
+// Snail variables
+let snailBodyLength = 7
+let nStartingSnails = 2
+let probNewSnail = 0.02
+let snails = []
+// Trail variables
+let viewTrail = true
+let trailWidth = 5
+let trailStep = trailWidth / 2
+let trailColor = [255, 255, 255, 50]
+let trailWobble = 1 / 4  // In fraction of pi
+// Shell variables
+let offsetX = nPixelsWidth / 2
+let offsetY = nPixelsHeight
+let shellTheta = 0
+let ampX = 250
+let ampY = 10
+// Other variable
+// let saveFrames = true
+let saveFrames = false
+let fps = 10
+let nFrames = 250
+
+class Snail {
+  constructor(x, y) {
+    // this.points = [[random(width), random(height)]]
+    this.x = x
+    this.y = y
+    this.trailPoints = [[this.x, this.y]]
+    this.trailTheta = random(2 * PI)
+  }
+  move() {
+    let dTheta = random(-PI * trailWobble / 2, PI * trailWobble / 2)
+    this.trailTheta += dTheta
+    let x2 = this.x + trailStep * cos(this.trailTheta)
+    let y2 = this.y + trailStep * sin(this.trailTheta)
+    this.trailPoints.push[[x2, y2]]
+    this.x = x2
+    this.y = y2
+    if (viewTrail) {
+      strokeWeight(5)
+      stroke.apply(null, trailColor)
+      point(x2, y2)
+    }
+  }
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(nPixelsWidth, nPixelsHeight)
+  frameRate(fps)
+  background(100)
+  for (let i = 0; i < nStartingSnails; i ++){
+    snails.push(new Snail(random(width / 2), random(height / 2), viewTrail))
+  }
   console.log('Press SPACE to stop looping or r to reset.')
 }
 
 function draw() {
-  background(220);
+  if (saveFrames) saveFrame()
+  if (random() < probNewSnail) {
+    snails.push(new Snail(random(width / 2), random(height / 2), viewTrail))
+  }
+  for (let snail of snails) {
+    snail.move()
+  }
+}
+
+function mousePressed() {
+  snails.push(new Snail(mouseX, mouseY, viewTrail))
+  console.log(`New snail at ${mouseX}, ${mouseY}. There are now ${snails.length} snails.`)
+}
+
+function drawShell() {
+  strokeWeight(1)
+  stroke(255, 0, 0)
+  // point(offsetX, offsetY)
+  let x = offsetX + ampX * cos(theta)
+  let y = offsetY + ampY * sin(theta)
+  stroke(0)
+  point(x, y)
+  theta += 0.05
+  ampX -= 0.1
+  // ampY -= 0
+  offsetY -= 0.1
 }
