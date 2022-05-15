77a5edd841a56f6542e8f235b0103d33315a34e5
cgusb <cgusbecker@gmail.com>
2022-05-15 00:29:53 -0400

    Add snail body

--- js/sketch.js
+++ js/sketch.js
@@ -2,8 +2,8 @@ let nPixelsWidth = 500
 let nPixelsHeight = nPixelsWidth
 // Snail variables
 let bodyColor = [255]
-let bodyLength = 7
-let bodyWidthMin = 7
+let bodyLength = 10
+let bodyWidthMin = 5
 let bodyWidthMax = 10
 let nStartingSnails = 2
 let probNewSnail = 0.02
@@ -27,11 +27,10 @@ let nFrames = 250
 
 class Snail {
   constructor(x, y) {
-    // this.points = [[random(width), random(height)]]
     this.x = x
     this.y = y
     this.bodyPoints = [[this.x, this.y]]
-    this.bodyWidthInc = floor(bodyLength / 2) / (bodyWidthMax - bodyWidthMin)
+    this.bodyWidthInc = (bodyWidthMax - bodyWidthMin) / floor(bodyLength / 2) 
     this.trailPoints = [[this.x, this.y]]
     this.trailTheta = random(2 * PI)
   }
@@ -41,40 +40,40 @@ class Snail {
     let x2 = this.x + trailStep * cos(this.trailTheta)
     let y2 = this.y + trailStep * sin(this.trailTheta)
     // Add point to array of trail points so it can be redrawn every frame
-    this.trailPoints.push[[x2, y2]]
+    this.trailPoints.push([x2, y2])
     // Add point to body if not full size or replace the first body point
-    if (this.trailPoints < bodyLength) {
-      this.bodyPoints.push[[x2, y2]]
+    if (this.trailPoints.length < bodyLength) {
+      this.bodyPoints.push([x2, y2])
     } else {
       // Remove first element of bodyPoints array
       this.bodyPoints.splice(0, 1)
       // Add new body point to the end of the bodyPoints array
-      this.bodyPoints.push[[x2, y2]]
+      this.bodyPoints.push([x2, y2])
     }
     this.x = x2
     this.y = y2
     this.drawBody()
     this.drawTrail()
   }
+  drawTrail() {
+    strokeWeight(trailWidth)
+    stroke.apply(null, trailColor)
+    for (let p of this.trailPoints) { 
+      point(p[0], p[1])
+    }
+  }
   drawBody() {
     stroke.apply(null, bodyColor)
+    let bodyWidth = bodyWidthMin
     for (let i = 0; i < this.bodyPoints.length; i ++) {
-      let p = this.bodyPoints[i]
-      let bodyWidth = bodyWidthMin
+      let p = this.bodyPoints[this.bodyPoints.length - i - 1]
       // Front of snail: increase body width by bodyWidthInc at each point. Back: decrease
-      if (i <= floor(bodyLength / 2) + 1) {
+      if (i <= floor(bodyLength / 2) - 1) {
         bodyWidth += this.bodyWidthInc
       } else (
         bodyWidth -= this.bodyWidthInc
       )
       strokeWeight(bodyWidth)
-      point(p[0], p[1])
-    }
-  }
-  drawTrail() {
-    strokeWeight(trailWidth)
-    stroke.apply(null, trailColor)
-    for (let p of this.trailPoints) { 
       point(p[0], p[1])
     }
   }
