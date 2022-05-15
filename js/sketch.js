982b9192571e445c57a93063ca8dcd826a5e1fee
cgusb <cgusbecker@gmail.com>
2022-05-15 15:05:08 -0400

    Add shells and tweak colors

--- js/sketch.js
+++ js/sketch.js
@@ -1,17 +1,23 @@
+/*
+Hello! Thank you for looking at my sketch!
+I have created a system that generates trail-leaving snails and sets them free across the canvas! Snails are generated as instances of the Snail class and are kept in the snails array. Each of the globally defined variables can be tweaked to change the bahavior of the snails.
+I hope you enjoy!
+<3 Gus
+*/
 let nPixelsWidth = 500
 let nPixelsHeight = nPixelsWidth
+let bgColor = ['#36393E']
 // Snail variables
-let bodyColor = [255]
-let bodyLength = 10
+let bodyLength = 20
 let bodyWidthMin = 5
-let bodyWidthMax = 10
+let bodyWidthMax = 15
 let nStartingSnails = 2
-let probNewSnail = 0.02
+let probNewSnail = 0.01
 let snails = []
 // Trail variables
 let trailWidth = 5
 let trailStep = trailWidth / 2
-let trailColor = [255, 255, 255, 50]
+let trailColor = [360, 0, 100, 25]  // 50% opacity white in in HSB
 let trailWobble = 1 / 4  // In fraction of pi
 // Shell variables
 let offsetX = nPixelsWidth / 2
@@ -29,6 +35,13 @@ class Snail {
   constructor(x, y) {
     this.x = x
     this.y = y
+    let hue = random(0, 360)
+    let shellSat = random(75, 100)
+    this.shellColor = [hue, shellSat, 100]
+    this.shellRadius = 1.5 * bodyWidthMax
+    this.shellPos = null
+    this.shellLayerOffset = 0.5
+    this.bodyColor = [hue, shellSat / 2, 100]
     this.bodyPoints = [[this.x, this.y]]
     this.bodyWidthInc = (bodyWidthMax - bodyWidthMin) / floor(bodyLength / 2) 
     this.trailPoints = [[this.x, this.y]]
@@ -52,8 +65,6 @@ class Snail {
     }
     this.x = x2
     this.y = y2
-    this.drawBody()
-    this.drawTrail()
   }
   drawTrail() {
     strokeWeight(trailWidth)
@@ -63,13 +74,14 @@ class Snail {
     }
   }
   drawBody() {
-    stroke.apply(null, bodyColor)
+    stroke.apply(null, this.bodyColor)
     let bodyWidth = bodyWidthMin
     for (let i = 0; i < this.bodyPoints.length; i ++) {
       let p = this.bodyPoints[this.bodyPoints.length - i - 1]
       // Front of snail: increase body width by bodyWidthInc at each point. Back: decrease
       if (i <= floor(bodyLength / 2) - 1) {
         bodyWidth += this.bodyWidthInc
+        if (i == floor(bodyLength / 2) - 1) this.shellPos = p
       } else (
         bodyWidth -= this.bodyWidthInc
       )
@@ -77,26 +89,43 @@ class Snail {
       point(p[0], p[1])
     }
   }
+  drawShell(x, y) {
+    if (this.shellPos != null) {
+      let [x, y] = this.shellPos
+      stroke(0)
+      strokeWeight(1)
+      fill.apply(null, this.shellColor)
+      circle(x, y, this.shellRadius)
+      for (let i = 1; i < 3; i ++) {
+        circle(x, y, 2 / (i * 3) * this.shellRadius)
+      }
+    }
+  }
 }
 
 function setup() {
   createCanvas(nPixelsWidth, nPixelsHeight)
   frameRate(fps)
-  background(100)
-  for (let i = 0; i < nStartingSnails; i ++){
-    snails.push(new Snail(random(width / 2), random(height / 2)))
-  }
+  colorMode(HSB, 360, 100, 100, 100)
+  resetSketch()
   console.log('Press SPACE to stop looping or r to reset.')
 }
 
 function draw() {
   if (saveFrames) saveFrame()
-  background(100)
+  background.apply(null, bgColor)
   if (random() < probNewSnail) {
-    snails.push(new Snail(random(width / 2), random(height / 2)))
+    snails.push(new Snail(random(width), random(height)))
   }
+  // Move snails and draw their trails
   for (let snail of snails) {
     snail.move()
+    snail.drawTrail()
+  }
+  // Draw snails & shells in a separate loop so every snail is above any trail
+  for (let snail of snails) {
+    snail.drawBody()
+    snail.drawShell()
   }
 }
 
@@ -105,16 +134,15 @@ function mousePressed() {
   console.log(`New snail at ${mouseX}, ${mouseY}. There are now ${snails.length} snails.`)
 }
 
-function drawShell() {
-  strokeWeight(1)
-  stroke(255, 0, 0)
-  // point(offsetX, offsetY)
-  let x = offsetX + ampX * cos(theta)
-  let y = offsetY + ampY * sin(theta)
-  stroke(0)
-  point(x, y)
-  theta += 0.05
-  ampX -= 0.1
-  // ampY -= 0
-  offsetY -= 0.1
+function resetSketch() {
+  snails = []
+  for (let i = 0; i < nStartingSnails; i ++){
+    snails.push(
+      new Snail(
+        random(width / 4, 3 / 4 * width), 
+        random(height / 4, 3 / 4 * height)
+      )
+    )
+  }
+  background.apply(null, bgColor)
 }
